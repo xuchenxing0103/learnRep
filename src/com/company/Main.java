811d5f8cfb694e0b63d11125bd3a6ac5dd74ecb1
guanghuixu <guanghuixu@yangqianguan.com>
2020-08-15 17:53:07 +0800

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,12 @@ package com.company;
 
 public class Main {
 
+    /**
+     * auth xugh
+     * @param args
+     */
     public static void main(String[] args) {
         int a = 1;
-
+        System.out.println("hello world");
     }
 }
